741215c0f0726a20c30e3e21bf84b9b4e3e5e19d
eklundto <eklundto@max-display013.desy.de>
2025-07-18 11:23:57 +0200

    Changed plotting function from plotting the density function to the area elements. This gives a better representation at log scales.

--- multiq_plot_result.m
+++ multiq_plot_result.m
@@ -14,7 +14,7 @@ for l =1:L
     if transform_s == 1
         title_str = append(title_str,"\int\rho_"+l+"(x)e^{-(xq^" + result.q_powers(l)/result.t_powers(l) + "\tau)^" + result.t_powers(l) + "}\mathrm{d}x +");
     else
-        title_str = append(title_str,"\int\rho_"+l+"(s)e^{-(s(q/q_0)^" + result.q_powers(l)/result.t_powers(l) + "(\tau/\tau_0))^" + result.t_powers(l) + "}\mathrm{d}x +");
+        title_str = append(title_str,"\int\rho_"+l+"(s)e^{-(s(q/q_0)^" + result.q_powers(l)/result.t_powers(l) + "(\tau/\tau_0))^" + result.t_powers(l) + "}\mathrm{d}s +");
     end
 end
 
@@ -27,9 +27,9 @@ c_map=parula(Q+1);
 
 % The label dicts work for diffusive/ballistic case. Will probably break
 % for other cases.
-y_labels = dictionary([1 0],{"Diffusive component","Ballistic component"});
-x_labels = dictionary([1 0 -1 -2],{['Diffusivity $[ \mathrm{\AA^{2}/s}]$'],['Velocity $[\mathrm{\AA/s}]$'],'Rel. rate $[\mathrm{s}^{-1}]$','Rel. rate [s^{-1}]'});
-label_keys = result.q_powers-result.t_powers;
+subtitles = dictionary([2 1 0],{"Diffusive component","Ballistic component","Relaxational component"});
+x_labels = dictionary([2 1 0],{['Diffusivity $[ \mathrm{\AA^{2}/s}]$'],['Velocity $[\mathrm{\AA/s}]$'],['Rel. rate $[\mathrm{s}^{-1}]$']});
+label_keys = result.q_powers./result.t_powers;
 
 figure('Position', [1 1 3*fsize fsize ]);
 tiles = tiledlayout(L,3,'padding','compact','TileSpacing','compact');
@@ -59,7 +59,7 @@ ylabel('$g_2$','interpreter','latex');
 
 f_ax=nexttile(2, [L 1]);
 
-lstyles = dictionary([1 2],{'--',':'});
+lstyles = ["--",":","-."];
 
 hold on
 for q = 1:Q
@@ -70,10 +70,10 @@ for q = 1:Q
     end
     f_est = sqrt((result.g2(q,:)-result.baseline(q)-1)/result.contrast(q));
     scatter(result.t, f_est, markersize,c_map(q,:),'HandleVisibility','off');
-    plot(result.fit_eval_t,result.f_fits(q,:),'-.','LineWidth',1,'Color',[c_map(q,:) 0.5],'HandleVisibility',lgd_vis,'DisplayName','Total fit');
+    plot(result.fit_eval_t,result.f_fits(q,:),'-','LineWidth',1,'Color',[c_map(q,:) 0.5],'HandleVisibility',lgd_vis,'DisplayName','Total fit');
     
     for l=1:L
-        plot(result.fit_eval_t,result.f_comp_fits(q,:,l),lstyles{l},'LineWidth',1,'Color',c_map(q,:),'HandleVisibility',lgd_vis,'DisplayName',y_labels{label_keys(l)});
+        plot(result.fit_eval_t,result.f_comp_fits(q,:,l),lstyles(l),'LineWidth',1,'Color',c_map(q,:),'HandleVisibility',lgd_vis,'DisplayName',subtitles{label_keys(l)});
     end
 end
 flgd = legend('Location','southwest','Interpreter','Latex');
@@ -92,29 +92,35 @@ for l=1:L
     q0=result.q_norm;
     tau0=result.t_norm;
 
+    y = result.w(1+M*(l-1):M*l)'.*result.component_dist(:,l);
+
     if transform_s == 1
-        x = result.s(1+M*(l-1):M*l)/(q0^(result.q_powers(l)/result.t_powers(l))*tau0);
+        x = (result.s(1+M*(l-1):M*l)-result.w(1+M*(l-1):M*l)/2)/(q0^(result.q_powers(l)/result.t_powers(l))*tau0);
         %dsdx = q0^result.q_powers(l)*tau0^result.t_powers(l)*result.t_powers(l)*x.^(result.t_powers(l)-1);
-        dsdx=q0^(result.q_powers(l)/result.t_powers(l))*tau0;
-        y= dsdx.*result.component_dist(:,l);
+        %dsdx=q0^(result.q_powers(l)/result.t_powers(l))*tau0;
+        % y= dsdx.*result.component_dist(:,l);
         %y = result.w(1+M*(l-1):M*l)'.*result.component_dist(:,l);
         xlab = x_labels(label_keys(l));
+        %ylabel("$\rho_" +l+"\Delta x$", 'interpreter','latex');
     else
-        x = result.s(1+M*(l-1):M*l);
-        y = result.component_dist(:,l);
+        x = result.s(1+M*(l-1):M*l)-result.w(1+M*(l-1):M*l)/2;
+        %y = result.w(1+M*(l-1):M*l)'.*result.component_dist(:,l);
         xlab = 's';
+        %ylabel("$\rho_" +l+"\Delta s$", 'interpreter','latex');
     end
 
-    scatter(x,y,'LineWidth',1);
+    [xs,ys] = stairs(x,y);%,'LineWidth',1);
+    area(xs,ys,0,'FaceColor','blue','FaceAlpha',0.2,'LineStyle',lstyles(l),'LineWidth',0.75,'EdgeColor','blue');
 
     ylim([0 max(y)]);
 
     xscale log;
 
     xlabel(xlab,'interpreter','latex');
-    ylabel("$\rho_" +l+"$", 'interpreter','latex');
+    ylabel("$\rho_" +l+"\Delta x$", 'interpreter','latex');
 
-    title(y_labels{label_keys(l)});
+
+    title(subtitles{label_keys(l)});
 end
 
 tit = sgtitle([regexprep(result.sample,'_','\\_') ' series ' num2str(result.series) ', $|f(q,\tau)|=' title_str{1}(1:end-1) '$']);
